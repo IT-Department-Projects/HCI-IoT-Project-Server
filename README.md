2a7f08fcf77ea5c03c673fa9fb940dac3953a0c7
Salman Shah <sbs.191197@gmail.com>
2018-03-14 19:44:31 +0530

    Code changes made 1. Updated README to include instructions on how to install the server dependencies 2. Added a sample code on how to detect images for Barack Obama. 3. Modified server code to have the Deep Learning Library used as well

--- README.md
+++ README.md
@@ -5,9 +5,65 @@
 * Aiman A.
 * Rashika C.
 * Aniket K.
+* Renu C.
 
 ## Setup
-* Run the following instructions in the terminal.
+
+### Libraries Used
+* gevent
+* Flask_HTTPAuth
+* face_recognition
+* Flask
+* flask_migrate
+
+### Instructions to install the Face Recognition Library
+
+* Make sure you have dlib installed with Python Bindings
+
+* To install dlib, make sure you have **Python 3 pre-installed**. This is usually built-in for Linux.
+
+* After that run the following commands on your terminal.
+```
+sudo apt-get install -y --fix-missing \
+    build-essential \
+    cmake \
+    gfortran \
+    git \
+    wget \
+    curl \
+    graphicsmagick \
+    libgraphicsmagick1-dev \
+    libatlas-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libgtk2.0-dev \
+    libjpeg-dev \
+    liblapack-dev \
+    libswscale-dev \
+    pkg-config \
+    python3-dev \
+    python3-numpy \
+    software-properties-common \
+    zip \
+&& apt-get clean && rm -rf /tmp/* /var/tmp/*
+```
+
+```
+cd ~ && \
+    mkdir -p dlib && \
+    git clone -b 'v19.9' --single-branch https://github.com/davisking/dlib.git dlib/ && \
+    cd  dlib/ 
+```
+
+```
+mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+cd ..
+python3 setup.py install --yes USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA
+```
+
+## Using Virtual Environment
+
+* It is preferable to use a Virtual Environment rather than use your core Python Environment. Run the following instructions in the terminal to create and install all dependencies in the Virtual Environment.
 
 ```bash
 virtualenv venv
@@ -15,7 +71,7 @@ pip install -r requirements.txt
 source venv/bin/activate
 ```
 
-* To start the server, you can run the following command:
+* After installing the dependencies all you need to do is start the server to get your code running. To start the server, you can run the following command:
 ```
 python server.py
 ```
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import json
-from flask import Flask, request, Response, render_template, abort, url_for
+from flask import Flask, request, Response, render_template, abort, url_for, jsonify, redirect
 from flask_httpauth import HTTPDigestAuth
 from flask_migrate import Migrate
 import gevent
 import time
 from flask_httpauth import HTTPDigestAuth
+
+import face_recognition
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 # Flask Variables
 app = Flask(__name__)
@@ -22,13 +26,67 @@ users = {
 }
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/image_recognition', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # The image file seems valid! Detect faces and return the result.
+            return detect_faces_in_image(file)
+
+    # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method="POST" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Upload">
+    </form>
+    '''
+
+'''
+Searches for image amongst current encoding values
+'''
+def detect_faces_in_image(file_stream):
+
+    # Load the uploaded image file
+    img = face_recognition.load_image_file(file_stream)
+    # Get face encodings for any faces in the uploaded image
+    unknown_face_encodings = face_recognition.face_encodings(img)
+
+    '''
+        To Do: 
+        Return List of names of files whose images 
+        are there in the given picture
+    '''
+
+    # Return the result of names of people as json
+    result = {
+        "": ""
+    }
+    return jsonify(result)
+
+
 # Authenticating users from Dictionary
 @auth.get_password
 def get_pw(username):
     if username in users:
         return users.get(username)
     return None
-
 
 @app.route('/')
 @auth.login_required
